05c85ad5dcc5433f538315383bfc83026c12c60f
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-15 22:51:18 +0000

    reverseInParenthesis solution to Arcade Intro from Code Signal

---
+++ CodeSignal/reverseInParenthesis.js
@@ -0,0 +1,14 @@
+function solution(inputString) {
+  while (true) {
+    var close = inputString.indexOf(')');
+    if (close === -1) break;
+    var open = inputString.substring(0, close).lastIndexOf('(');
+    var start = inputString.substring(0, open);
+    var middle = inputString.substring(open + 1, close).split('').reverse().join('');
+    var end = inputString.substring(close + 1, inputString.length);
+    inputString = start + middle + end;
+  }
+  return inputString;
+}
+
+solution('(bar)');
