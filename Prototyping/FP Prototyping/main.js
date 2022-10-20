07c1e2f3511c46bdd79a8dfd6c837dc459166975
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-20 23:42:33 +0000

    Testing adding event listener to button to click input -- missing link to main.js file

--- Prototyping/FP Prototyping/main.js
+++ Prototyping/FP Prototyping/main.js
@@ -1,8 +1,23 @@
-// const $uploadInput = document.querySelector('.upload');
-// const $captureInput = document.querySelector('.capture');
-// const $uploadButton = document.querySelector('.upload-button');
-// const $captureButton = document.querySelector('.capture-button');
+const $uploadInput = document.querySelector('.upload');
+const $captureInput = document.querySelector('.capture');
+const $uploadButton = document.querySelector('.upload-button');
+const $captureButton = document.querySelector('.capture-button');
+const $inputImage = document.querySelector('.input-image');
 
-// $uploadButton.addEventListener('click', () => {
-//   console.log('clicked');
-// });
+$uploadButton.addEventListener('click', clickInput);
+
+function clickInput() {
+  $uploadInput.click();
+}
+
+$captureButton.addEventListener('click', clickCapture);
+
+function clickCapture() {
+  $captureInput.click();
+}
+
+$captureInput.addEventListener('onsubmit', updateImage);
+
+function updateImage() {
+  $inputImage.setAttribute('src', $captureInput.value);
+}
