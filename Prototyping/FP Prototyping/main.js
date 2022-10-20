1920fddbfb8fac372aebd8c09ce012fa990c7469
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-20 22:03:21 +0000

    Working on using button to click input element

---
+++ Prototyping/FP Prototyping/main.js
@@ -0,0 +1,8 @@
+// const $uploadInput = document.querySelector('.upload');
+// const $captureInput = document.querySelector('.capture');
+// const $uploadButton = document.querySelector('.upload-button');
+// const $captureButton = document.querySelector('.capture-button');
+
+// $uploadButton.addEventListener('click', () => {
+//   console.log('clicked');
+// });
