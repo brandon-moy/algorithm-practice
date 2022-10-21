3e79ad3153097fa420af3531fc33408c78659beb
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-21 19:26:38 +0000

    Added notes for what I want for the next step of using the image

--- Prototyping/FP Prototyping/main.js
+++ Prototyping/FP Prototyping/main.js
@@ -19,6 +19,13 @@ function clickCapture() {
 $captureInput.addEventListener('change', loadImage);
 $uploadInput.addEventListener('change', loadImage);
 
+// will probably want to hide buttons, display modal with new buttons
+// modal will show image and then there should be two buttons
+// Do you want to use this image?
+// 1 - Yes
+// 2 - Retake and it should allow the user to retake the picture or reupload by
+// activating the input again
+
 function loadImage(event) {
   $inputImage.setAttribute('src', URL.createObjectURL(event.target.files[0]));
 }
