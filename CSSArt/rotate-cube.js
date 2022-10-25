127a0e09d1aca4ee6cdee1fc9e4bbdced555a19f
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-25 23:18:05 +0000

    Modified border object with border width sizes and added modifications to DOM elements based on position of cube

--- CSSArt/rotate-cube.js
+++ CSSArt/rotate-cube.js
@@ -3,28 +3,36 @@ const $cube2 = document.querySelector('.cube2');
 const $start = document.querySelector('.start');
 const $stop = document.querySelector('.stop');
 const $cubeFaceTop = document.querySelector('.cube-face-top');
+const $cubeFaceBottom = document.querySelector('.cube-face-bottom');
+const $cubeFaceFront = document.querySelector('.cube-face-front');
+const $cubeFaceRight = document.querySelector('.cube-face-right');
+const $cubeFaceBack = document.querySelector('.cube-face-back');
+const $cubeFaceLeft = document.querySelector('.cube-face-left');
 let count = 1;
 let intervalId = null;
 let borderIntervalId = null;
-let position = 1;
+let position = 0;
 const borders = {
-  top: ['8px 8px 3px 3px', '3px 8px 8px 3px', '8px 3px 3px 8px',
-    '3px 3px 8px 8px']
-
+  top: ['6px 6px 1px 1px', '1px 6px 6px 1px',
+    '1px 1px 6px 6px', '6px 1px 1px 6px'],
+  bottom: ['6px 1px 1px 6px', '1px 1px 6px 6px',
+    '1px 6px 6px 1px', '6px 6px 1px 1px'],
+  left: ['1px 6px 1px 1px', '1px 1px 1px 6px',
+    '1px 6px 1px 1px', '1px 1px 1px 6px'],
+  right: ['1px 1px 1px 6px', '1px 6px 1px 1px',
+    '1px 1px 1px 6px', '1px 6px 1px 1px']
 };
-
-// currently does nothing... need to understand how to update border-width with
-// value?
 
 function borderSet() {
   borderIntervalId = setInterval(() => {
-    if (position === 3) {
-      position = 0;
-      $cubeFaceTop.style.borderWidth = borders.top[position];
-    } else {
-      $cubeFaceTop.style.borderWidth = borders.top[position];
-    }
     position++;
+    if (position === 4) position = 0;
+    $cubeFaceTop.style.borderWidth = borders.top[position];
+    $cubeFaceBottom.style.borderWidth = borders.bottom[position];
+    $cubeFaceFront.style.borderWidth = borders.left[position];
+    $cubeFaceBack.style.borderWidth = borders.left[position];
+    $cubeFaceRight.style.borderWidth = borders.right[position];
+    $cubeFaceLeft.style.borderWidth = borders.right[position];
   }, 3000);
 }
 
