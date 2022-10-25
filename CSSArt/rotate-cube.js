4afc60d2aa4185075cdba4b7ba6e82b7c7314430
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-25 16:03:36 +0000

    Created two cubes that rotate in opposite directions using set interval

--- CSSArt/rotate-cube.js
+++ CSSArt/rotate-cube.js
@@ -1,1 +1,15 @@
-// const $cube = document.querySelector('.cube');
+const $cube = document.querySelector('.cube');
+const $cube2 = document.querySelector('.cube2');
+const $stop = document.querySelector('.stop');
+let count = 1;
+
+const rotate = setInterval(() => {
+  const amount = 45 + (count * 90);
+  $cube.style.transform = 'translateZ(-100px) rotateX(-45deg) rotateY(' + amount + 'deg)';
+  $cube2.style.transform = 'translateZ(-100px) rotateX(-45deg) rotateY(-' + amount + 'deg)';
+  count++;
+}, 2000);
+
+$stop.addEventListener('click', () => {
+  clearInterval(rotate);
+});
