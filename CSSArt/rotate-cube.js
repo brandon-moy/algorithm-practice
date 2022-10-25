f79fb5d805430b63dc8178c36a0c69a56c4d2334
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-25 21:14:56 +0000

    Styled cube to look like webpack and adjusted transform after moving cube2 into cube

--- CSSArt/rotate-cube.js
+++ CSSArt/rotate-cube.js
@@ -1,15 +1,24 @@
 const $cube = document.querySelector('.cube');
 const $cube2 = document.querySelector('.cube2');
+const $start = document.querySelector('.start');
 const $stop = document.querySelector('.stop');
 let count = 1;
+let intervalId = null;
 
-const rotate = setInterval(() => {
-  const amount = 45 + (count * 90);
-  $cube.style.transform = 'translateZ(-100px) rotateX(-45deg) rotateY(' + amount + 'deg)';
-  $cube2.style.transform = 'translateZ(-100px) rotateX(-45deg) rotateY(-' + amount + 'deg)';
-  count++;
-}, 2000);
-
+function rotate() {
+  intervalId = setInterval(() => {
+    const amount = 45 + (count * 90);
+    const cube2amount = (count * 180);
+    $cube.style.transform = 'translateZ(-100px) rotateX(-45deg) rotateY(' + amount + 'deg)';
+    $cube2.style.transform = 'rotateY(-' + cube2amount + 'deg)';
+    count++;
+  }, 2000);
+}
 $stop.addEventListener('click', () => {
-  clearInterval(rotate);
+  clearInterval(intervalId);
+  intervalId = null;
 });
+
+$start.addEventListener('click', rotate);
+
+rotate();
