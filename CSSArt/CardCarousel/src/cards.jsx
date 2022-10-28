634a99d01aeb25307de8502d283a4580aa686dc0
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-28 15:27:54 +0000

    Create animation to rotate carousel and added additional information to add click function to linkedin

--- CSSArt/CardCarousel/src/cards.jsx
+++ CSSArt/CardCarousel/src/cards.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 
 export default class Cards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { links: [] };
+  }
+
+  handleClick(event) {
+    // console.log(event.target.closest('.card'));
+    // window.open('https://www.linkedin.com/in/ashleylho/');
+    // how can i put linkedinlinks into array and click div to open link?
+  }
+
+  componentDidMount() {
+    const links = this.props.cards.map(obj => obj.linked);
+    this.setState({ links });
+  }
+
   render() {
     return (
       <div className="scene">
@@ -8,11 +25,14 @@ export default class Cards extends React.Component {
           {
             this.props.cards.map(image => {
               return (
-                <div className="card" key={image.number}>
-                  <div className="front"></div>
-                  <div className="back">
-                    {/* <img src={image.background}></img> */}
+                <div className="card" key={image.number} onClick={this.handleClick}>
+                  <div className="front">
+                    <img className="student-image" src={image.front}></img>
+                    <h1 className="student-name">{image.name}</h1>
                   </div>
+                  {/* <div className="back"> */}
+                  {/* <img src={image.background}></img> */}
+                  {/* </div> */}
                 </div>
               );
             })
--- CSSArt/CardCarousel/src/index.jsx
+++ CSSArt/CardCarousel/src/index.jsx
@@ -3,18 +3,78 @@ import ReactDOM from 'react-dom/client';
 import Cards from './cards';
 
 const cards = [
-  { number: 0, name: 'Markus', background: 'images/logo.png' },
-  { number: 1, name: 'Jiheng', background: 'images/logo.png' },
-  { number: 2, name: 'Brandon', background: 'images/logo.png' },
-  { number: 3, name: 'Eugene', background: 'images/logo.png' },
-  { number: 4, name: 'Anthony', background: 'images/logo.png' },
-  { number: 5, name: 'Daisy', background: 'images/logo.png' },
-  { number: 6, name: 'Derrick', background: 'images/logo.png' },
-  { number: 7, name: 'Jake', background: 'images/logo.png' },
-  { number: 8, name: 'Jianni', background: 'images/logo.png' },
-  { number: 9, name: 'Quan', background: 'images/logo.png' },
-  { number: 10, name: 'Matt', background: 'images/logo.png' },
-  { number: 11, name: 'Ashley', background: 'images/logo.png' }
+  {
+    number: 0,
+    name: 'Markus',
+    front: 'images/markus.png',
+    linked: 'https://www.linkedin.com/in/markusdoan-le/'
+  },
+  {
+    number: 1,
+    name: 'Jiheng',
+    front: 'images/jiheng.png',
+    linked: 'https://www.linkedin.com/in/jiheng-nie/'
+  },
+  {
+    number: 2,
+    name: 'Brandon',
+    front: 'images/brandon.png',
+    linked: 'https://www.linkedin.com/in/brandon-k-moy/'
+  },
+  {
+    number: 3,
+    name: 'Eugene',
+    front: 'images/eugene.png',
+    linked: 'https://www.linkedin.com/in/eugenepark215/'
+  },
+  {
+    number: 4,
+    name: 'Anthony',
+    front: 'images/anthony.png',
+    linked: 'https://www.linkedin.com/in/anthonyisaiahurbina/'
+  },
+  {
+    number: 5,
+    name: 'Daisy',
+    front: 'images/daisy.png',
+    linked: 'https://www.linkedin.com/in/daisy-yoo/'
+  },
+  {
+    number: 6,
+    name: 'Derrick',
+    front: 'images/derrick.png',
+    linked: 'https://www.linkedin.com/in/derrick-fleming/'
+  },
+  {
+    number: 7,
+    name: 'Jake',
+    front: 'images/jake.png',
+    linked: 'https://www.linkedin.com/in/jacob-husband/'
+  },
+  {
+    number: 8,
+    name: 'Jianni',
+    front: 'images/jianni.png',
+    linked: 'https://www.linkedin.com/in/jiannicariaga/'
+  },
+  {
+    number: 9,
+    name: 'Quan',
+    front: 'images/quan.png',
+    linked: 'https://www.linkedin.com/in/quannampham/'
+  },
+  {
+    number: 10,
+    name: 'Matt',
+    front: 'images/matt.png',
+    linked: 'https://www.linkedin.com/in/matthew-kweon/'
+  },
+  {
+    number: 11,
+    name: 'Ashley',
+    front: 'images/ashley.png',
+    linked: 'https://www.linkedin.com/in/ashleylho/'
+  }
 ];
 
 const container = document.querySelector('#root');
