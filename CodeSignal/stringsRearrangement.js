6198185f6b1722d001418e20d20056f6596525b0
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-05 17:32:02 +0000

    stringsRearrangement solution to ArcadeIntro from CodeSignal

---
+++ CodeSignal/stringsRearrangement.js
@@ -0,0 +1,39 @@
+function solution(inputArray) {
+  let result = false;
+
+  function check(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+      let diffCounter = 0;
+      for (let j = 0; j < array[i].length; j++) {
+        if (array[i][j] !== array[i + 1][j]) {
+          diffCounter++;
+        }
+      }
+      if (diffCounter !== 1) return false;
+    }
+    return true;
+  }
+
+  function loopArray(array, mergeArray) {
+    if (mergeArray.length === inputArray.length) {
+      return check(mergeArray);
+    } else {
+      for (const i in array) {
+        if (
+          loopArray(
+            array.slice(0, +i).concat(array.slice(+i + 1, array.length)),
+            [...mergeArray, array[i]]
+          )
+        ) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+
+  result = loopArray(inputArray, []);
+  return result;
+}
+
+solution(['q', 'q']);
