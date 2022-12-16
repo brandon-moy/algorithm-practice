7c8496b4c4b8ce9543bddc9b75320c76595c19d5
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-16 16:42:38 +0000

    Calculated Big O Notation for Add.js

--- CodeSignal/add.js
+++ CodeSignal/add.js
@@ -1,8 +1,10 @@
 // Write a function that returns the sum of two numbers.
 
 function solution(param1, param2) {
-  var sum = param1 + param2;
-  return sum;
+  var sum = param1 + param2; // 2 * 1 = 0(1)
+  return sum; // 1 * 1 = 0(1)
 }
+
+// Big O Notation for add: 0(1) constant time
 
 solution(1, 2);
