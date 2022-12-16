2f245a9516a76901e4825e7399d94f412e6dfc0e
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-16 16:44:35 +0000

    Calculated Big O Notation for century.js

--- CodeSignal/century.js
+++ CodeSignal/century.js
@@ -1,13 +1,15 @@
 // Given a year, return the century it is in.The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
 
 function solution(year) {
-  var century = year / 100;
-  var checkCentury = Math.floor(century);
-  if (century === checkCentury) {
-    return checkCentury;
+  var century = year / 100; // 2 * 1 = 0(1)
+  var checkCentury = Math.floor(century); // 3 * 1 = 0(1)
+  if (century === checkCentury) { // 3 * 1 = 0(1)
+    return checkCentury; // 1 * 1 = 0(1)
   } else {
-    return checkCentury + 1;
+    return checkCentury + 1; // 2 * 1 = 0(1)
   }
 }
+
+// Big O Notation = 0(1) constant time
 
 solution(1994);
