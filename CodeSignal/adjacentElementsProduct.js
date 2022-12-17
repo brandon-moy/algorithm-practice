e13b13faa96c32ae8d374077c717c6691c31b00d
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-17 17:42:49 +0000

    Calculated Big O Notation for adjacentElementsProduct.js

--- CodeSignal/adjacentElementsProduct.js
+++ CodeSignal/adjacentElementsProduct.js
@@ -1,19 +1,24 @@
 // Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
 
 function solution(inputArray) {
-  var output = inputArray[0] * inputArray[1];
-  for (var i = 1; i < inputArray.length; i++) {
-    var currentProduct = 0;
-    if (inputArray[i + 1] !== undefined) {
-      currentProduct = inputArray[i] * inputArray[i + 1];
+  var output = inputArray[0] * inputArray[1]; // 4 * 1 = 0(4) ~= 0(1)
+  for (
+    var i = 1; // 1 * 1 = 0(1)
+    i < inputArray.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    var currentProduct = 0; // 1 * n = 0(n)
+    if (inputArray[i + 1] !== undefined) { // 3 * n = 0(2n) ~= 0(n)
+      currentProduct = inputArray[i] * inputArray[i + 1]; // 4 * n = 0(4n) ~= 0(n)
     } else {
-      break;
+      break; // 1 * n = 0(n)
     }
-    if (currentProduct > output) {
-      output = currentProduct;
+    if (currentProduct > output) { // 1 * n = 0(n)
+      output = currentProduct; // 1 * n = 0(n)
     }
   }
-  return output;
+  return output; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution([1, 2, 3]);
