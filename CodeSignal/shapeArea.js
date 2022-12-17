f8160a5eacd0258c72594be84df53058781b29df
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-17 17:44:01 +0000

    Calculated Big O Notation for shapeArea.js

--- CodeSignal/shapeArea.js
+++ CodeSignal/shapeArea.js
@@ -1,11 +1,13 @@
 // Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
 
 function solution(n) {
-  if (n <= 1) {
-    return 1;
+  if (n <= 1) { // 1 * 1 = 0(1)
+    return 1; // 1 * 1 = 0(1)
   }
-  var area = (n * n) + (n - 1) * (n - 1);
-  return area;
+  var area = (n * n) + (n - 1) * (n - 1); // 6 * 1 = 0(1)
+  return area; // 1 * 1 = 0(1)
 }
+
+// Big O Notation 0(1) constant time
 
 solution(4);
