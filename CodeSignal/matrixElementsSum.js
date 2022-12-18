447fce304f6fab89de35e55eadf6a5bd4410d045
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-18 16:30:31 +0000

    Calculated Big O Notation for matrixElementsSum.js

--- CodeSignal/matrixElementsSum.js
+++ CodeSignal/matrixElementsSum.js
@@ -1,18 +1,26 @@
 function solution(matrix) {
-  var output = 0;
-  for (var i = 0; i < matrix.length; i++) {
-    for (var j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] === 0) {
-        if (matrix[i + 1] !== undefined) {
-          matrix[i + 1].splice(j, 1, 0);
+  var output = 0; // 1 * 1 = 0(1)
+  for (
+    var i = 0; // 1 * 1 = 0(1)
+    i < matrix.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    for (
+      var j = 0; // 1 * 1 * n = 0(n)
+      j < matrix[i].length; // 3 * n * n = 0(3n^2) ~= 0(n^2)
+      j++) { // 2 * n * n = 0(2n^2) ~= 0(n^2)
+      if (matrix[i][j] === 0) { // 2 * n * n = 0(2n^2) ~= 0(n^2)
+        if (matrix[i + 1] !== undefined) { // 2 * n * n = 0(2n^2) ~= 0(n^2)
+          matrix[i + 1].splice(j, 1, 0); // 4 * n * n = 0(4n^2) ~= 0(n^2)
         }
       } else {
-        output += matrix[i][j];
+        output += matrix[i][j]; // 3 * n * n = 0(3n^2) ~= 0(n^2)
       }
     }
   }
-  return output;
+  return output; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n^2) quadratic time
 
 solution([[0, 1, 1, 2],
   [0, 5, 0, 0],
