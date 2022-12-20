3e045befc1236c50752ee3a8b71d188c5b3095cc
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-20 20:07:26 +0000

    Calculated Big O Notation for alternatingSums.js

--- CodeSignal/alternatingSums.js
+++ CodeSignal/alternatingSums.js
@@ -1,18 +1,23 @@
 function solution(a) {
-  var output = [];
-  var team1 = 0;
-  var team2 = 0;
-  for (var i = 0; i < a.length; i++) {
-    if (i % 2) {
-      team2 += a[i];
+  var output = []; // 1 * 1 = 0(1)
+  var team1 = 0; // 1 * 1 = 0(1)
+  var team2 = 0; // 1 * 1 = 0(1)
+  for (
+    var i = 0; // 1 * 1 = 0(1)
+    i < a.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    if (i % 2) { // 3 * n = 0(3n) ~= 0(n)
+      team2 += a[i]; // 3 * n = 0(3n) ~= 0(n)
     } else {
-      team1 += a[i];
+      team1 += a[i]; // 3 * n = 0(3n) ~= 0(n)
     }
   }
-  output.push(team1);
-  output.push(team2);
-  return output;
+  output.push(team1); // 2 * 1 = 0(2) ~= 0(1)
+  output.push(team2); // 2 * 1 = 0(2) ~= 0(1)
+  return output; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution([50, 60, 60, 45, 70]);
 
