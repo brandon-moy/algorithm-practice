8d27b9aa4d36c4eba9961aa5eac86bee7abf907e
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-21 00:03:37 +0000

    Calculated Big O Notation for addBorder.js

--- CodeSignal/addBorder.js
+++ CodeSignal/addBorder.js
@@ -1,17 +1,25 @@
 function solution(picture) {
-  var length = picture[0].length;
-  var borderPic = [];
-  var border = '';
-  for (var i = 0; i < length + 2; i++) {
-    border += '*';
+  var length = picture[0].length; // 3 * 1 = 0(3) ~= 0(1)
+  var borderPic = []; // 1 * 1 = 0(1)
+  var border = ''; // 1 * 1 = 0(1)
+  for (
+    var i = 0; // 1 * 1 = 0(1)
+    i < length + 2; // 2 * n = 0(2n) ~= 0(m)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    border += '*'; // 2 * n = 0(2n) ~= 0(n)
   }
-  for (var j = 0; j < picture.length; j++) {
-    borderPic[j] = '*' + picture[j] + '*';
+  for (
+    var j = 0; // 1 * 1 = 0(1)
+    j < picture.length; // 2 * n = 0(2n) ~= 0(m)
+    j++) { // 2 * n = 0(2n) ~= 0(m)
+    borderPic[j] = '*' + picture[j] + '*'; // 3 * n = 0(3n) ~= 0(n)
   }
-  borderPic.unshift(border);
-  borderPic.push(border);
-  return borderPic;
+  borderPic.unshift(border); // 2 * 1 = 0(2) ~= 0(1)
+  borderPic.push(border); // 2 * 1 = 0(2) ~= 0(1)
+  return borderPic; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 var picture = ['abc',
   'ded'];
