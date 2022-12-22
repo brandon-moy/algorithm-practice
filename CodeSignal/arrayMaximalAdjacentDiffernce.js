9091a7834c63f31fb25697042bd2d05c4ac8faea
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-22 18:57:23 +0000

    Calculated Big O Notation for arrayMaximalAdjacentDifference.js

--- CodeSignal/arrayMaximalAdjacentDiffernce.js
+++ CodeSignal/arrayMaximalAdjacentDiffernce.js
@@ -1,17 +1,22 @@
 function solution(inputArray) {
-  let maxDiff = 0;
-  for (let i = 0; i < inputArray.length - 1; i++) {
-    let diff = 0;
-    if (inputArray[i] < inputArray[i + 1]) {
-      diff = inputArray[i + 1] - inputArray[i];
+  let maxDiff = 0; // 1 * 1 = 0(1)
+  for (
+    let i = 0; // 1 * 1 = 0(1)
+    i < inputArray.length - 1; // 3 * n = 0(3n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    let diff = 0; // 1 * n = 0(n)
+    if (inputArray[i] < inputArray[i + 1]) { // 4 * n = 0(4n) ~= 0(n)
+      diff = inputArray[i + 1] - inputArray[i]; // 3 * n = 0(3n) ~= 0(n)
     } else {
-      diff = inputArray[i] - inputArray[i + 1];
+      diff = inputArray[i] - inputArray[i + 1]; // 3 * n = 0(3n) ~= 0(n)
     }
-    if (diff > maxDiff) {
-      maxDiff = diff;
+    if (diff > maxDiff) { // 1 * n = 0(n)
+      maxDiff = diff; // 1 * n = 0(n)
     }
   }
-  return maxDiff;
+  return maxDiff; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution([0, 0]);
