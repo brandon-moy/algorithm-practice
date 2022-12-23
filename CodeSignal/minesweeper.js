92c6e235d5aad0833471b87c62d512427b59b201
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-23 15:39:03 +0000

    Calculated Big O Notation for minesweeper.js

--- CodeSignal/minesweeper.js
+++ CodeSignal/minesweeper.js
@@ -1,42 +1,48 @@
 function solution(matrix) {
-  const output = [];
+  const output = []; // 1 * 1 = 0(1)
 
-  for (let i = 0; i < matrix.length; i++) {
-    output.push([]);
-    for (let j = 0; j < matrix[i].length; j++) {
-      let count = 0;
-      if (i > 0) {
-        if (matrix[i - 1][j]) {
-          count += 1;
+  for (
+    let i = 0; // 1 * 1 = 0(1)
+    i < matrix.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    output.push([]); // 2 * n = 0(2n) ~= 0(n)
+    for (
+      let j = 0; // 1 * n = 0(n)
+      j < matrix[i].length; // 3 * n * n = 0(3n^2) ~= 0(n^2)
+      j++) { // 2 * n * n = 0(2n^2) ~= 0(n^2)
+      let count = 0; // 1 * n * n = 0(n^2)
+      if (i > 0) { // 1 * n * n = 0(n^2)
+        if (matrix[i - 1][j]) { // 3 * n * n = 0(3n^2) ~= 0(n^2)
+          count += 1; // 3 * n * n = 0(3n^2) ~= 0(n^2)
         }
-        if (matrix[i - 1][j + 1]) {
-          count += 1;
+        if (matrix[i - 1][j + 1]) { // 4 * n * n = 0(4n^2) ~= 0(n^2)
+          count += 1; // 3 * n * n = 0(3n^2) ~= 0(n^2)
         }
-        if (matrix[i - 1][j - 1]) {
-          count += 1;
-        }
-      }
-      if (i < matrix.length - 1) {
-        if (matrix[i + 1][j]) {
-          count += 1;
-        }
-        if (matrix[i + 1][j + 1]) {
-          count += 1;
-        }
-        if (matrix[i + 1][j - 1]) {
-          count += 1;
+        if (matrix[i - 1][j - 1]) { // 4 * n * n = 0(4n^2) ~= 0(n^2)
+          count += 1; // 3 * n * n = 0(3n^2) ~= 0(n^2)
         }
       }
-      if (matrix[i][j + 1]) {
-        count += 1;
+      if (i < matrix.length - 1) { // 3 * n * n = 0(3n^2) ~= 0(n^2)
+        if (matrix[i + 1][j]) { // 3 * n * n = 0(3n^2) ~= 0(n^2)
+          count += 1; // 3 * n * n = 0(3n^2) ~= 0(n^2)
+        }
+        if (matrix[i + 1][j + 1]) { // 4 * n * n = 0(4n^2) ~= 0(n^2)
+          count += 1; // 3 * n * n = 0(3n^2) ~= 0(n^2)
+        }
+        if (matrix[i + 1][j - 1]) { // 4 * n * n = 0(4n^2) ~= 0(n^2)
+          count += 1; // 3 * n * n = 0(3n^2) ~= 0(n^2)
+        }
       }
-      if (matrix[i][j - 1]) {
-        count += 1;
+      if (matrix[i][j + 1]) { // 3 * n * n = 0(3n^2) ~= 0(n^2)
+        count += 1; // 3 * n * n = 0(3n^2) ~= 0(n^2)
       }
-      output[i].push(count);
+      if (matrix[i][j - 1]) { // 3 * n * n = 0(3n^2) ~= 0(n^2)
+        count += 1; // 3 * n * n = 0(3n^2) ~= 0(n^2)
+      }
+      output[i].push(count); // 4 * n * n = 0(4n^2) ~= 0(n^2)
     }
   }
-  return output;
+  return output; // 1 * 1 = 0(1)
 }
 
 solution([[true, false, false], [false, true, false], [false, false, false]]);
