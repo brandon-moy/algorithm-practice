ef42ec262ee90933f8d5ce9629e04fe831cc5a7f
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-23 15:41:49 +0000

    Calculated Big O Notation for evenDigitsOnly.js

--- CodeSignal/evenDigitsOnly.js
+++ CodeSignal/evenDigitsOnly.js
@@ -1,11 +1,16 @@
 function solution(n) {
-  const string = n.toString();
-  for (let i = 0; i < string.length; i++) {
-    if (parseInt(string[i]) % 2) {
-      return false;
+  const string = n.toString(); // 3 * 1 = 0(3) ~= 0(1)
+  for (
+    let i = 0; // 1 * 1 = 0(1)
+    i < string.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    if (parseInt(string[i]) % 2) { // 3 * n = 0(3n) ~= 0(n)
+      return false; // 1 * 1 = 0(1)
     }
   }
-  return true;
+  return true; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution(62236);
