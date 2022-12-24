aa7654119d7676cc53b52cee194fe2211a630205
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-24 19:12:26 +0000

    Calculated Big O Notation for chessBoardCellColor.js

--- CodeSignal/chessBoardCellColor.js
+++ CodeSignal/chessBoardCellColor.js
@@ -1,35 +1,39 @@
 function solution(cell1, cell2) {
-  const group1 = ['A', 'C', 'E', 'G'];
-  let cell1color = '';
-  let cell2color = '';
-  if (group1.includes(cell1[0])) {
-    if (parseInt(cell1[1] % 2)) {
-      cell1color = 'tan';
+  const group1 = ['A', 'C', 'E', 'G']; // 1 * 1 = 0(1)
+  let cell1color = ''; // 1 * 1 = 0(1)
+  let cell2color = ''; // 1 * 1 = 0(1)
+  if (group1.includes(cell1[0])) { // 3 * 1 = 0(3) ~= 0(1)
+    if (parseInt(cell1[1] % 2)) { // 4 * 1 = 0(4) ~= 0(1)
+      cell1color = 'tan'; // 1 * 1 = 0(1)
     } else {
-      cell1color = 'brown';
+      cell1color = 'brown'; // 1 * 1 = 0(1)
     }
   } else {
-    if (parseInt(cell1[1] % 2)) {
-      cell1color = 'brown';
+    if (parseInt(cell1[1] % 2)) { // 4 * 1 = 0(4) ~= 0(1)
+      cell1color = 'brown'; // 1 * 1 = 0(1)
     } else {
-      cell1color = 'tan';
+      cell1color = 'tan'; // 1 * 1 = 0(1)
     }
   }
-  if (group1.includes(cell2[0])) {
-    if (parseInt(cell2[1] % 2)) {
-      cell2color = 'tan';
+  if (group1.includes(cell2[0])) { // 3 * 1 = 0(3) ~= 0(1)
+    if (parseInt(cell2[1] % 2)) { // 4 * 1 = 0(4) ~= 0(1)
+      cell2color = 'tan'; // 1 * 1 = 0(1)
     } else {
-      cell2color = 'brown';
+      cell2color = 'brown'; // 1 * 1 = 0(1)
     }
   } else {
-    if (parseInt(cell2[1] % 2)) {
-      cell2color = 'brown';
+    if (parseInt(cell2[1] % 2)) { // 4 * 1 = 0(4) ~= 0(1)
+      cell2color = 'brown'; // 1 * 1 = 0(1)
     } else {
-      cell2color = 'tan';
+      cell2color = 'tan'; // 1 * 1 = 0(1)
     }
   }
-  if (cell1color === cell2color) return true;
-  return false;
+  if (cell1color === cell2color) { // 3 * 1 = 0(3) ~= 0(1)
+    return true; // 1 * 1 = 0(1)
+  }
+  return false; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(1) constant time
 
 solution('A1', 'H3');
