68da82d7edcd211eb9d7638f6f387be955559bcf
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-24 19:07:38 +0000

    Calculated Big O Notation for alphabeticShift.js

--- CodeSignal/alphabeticShift.js
+++ CodeSignal/alphabeticShift.js
@@ -1,14 +1,19 @@
 function solution(inputString) {
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  let output = '';
-  for (let i = 0; i < inputString.length; i++) {
-    let position = alphabet.indexOf(inputString[i]) + 1;
-    if (position === 26) {
-      position = 0;
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; // 1 * 1 = 0(1)
+  let output = ''; // 1 * 1 = 0(1)
+  for (
+    let i = 0; // 1 * 1 = 0(1)
+    i < inputString.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    let position = alphabet.indexOf(inputString[i]) + 1; // 4 * n = 0(4n) ~= 0(n)
+    if (position === 26) { // 2 * n = 0(2n) ~= 0(n)
+      position = 0; // 1 * 1 = 0(1)
     }
-    output += alphabet[position];
+    output += alphabet[position]; // 3 * n = 0(3n) ~= 0(n)
   }
-  return output;
+  return output; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution('crazy');
