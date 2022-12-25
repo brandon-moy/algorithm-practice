423d43076e1697d96bd4046cdccedd6c5ec163d9
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-25 18:47:34 +0000

    Calculated Big O Notation for depositProfit.js

--- CodeSignal/depositProfit.js
+++ CodeSignal/depositProfit.js
@@ -1,12 +1,14 @@
 function solution(deposit, rate, threshold) {
-  let growth = deposit;
-  const realRate = 1 + (rate / 100);
-  let years = 0;
-  while (growth < threshold) {
-    growth = growth * realRate;
-    years++;
+  let growth = deposit; // 2 * 1 = 0(3) ~= 0(1)
+  const realRate = 1 + (rate / 100); // 4 * 1 = 0(4) ~= 0(1)
+  let years = 0; // 1 * 1 = 0(1)
+  while (growth < threshold) { // 3 * n = 0(3n) ~= 0(n)
+    growth = growth * realRate; // 4 * n = 0(4n) ~= 0(n)
+    years++; // 2 * n = 0(2n) ~= 0(n)
   }
-  return years;
+  return years; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution(100, 20, 170);
