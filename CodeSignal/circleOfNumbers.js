a10e6f5e1dd0fa080820bd159c6f42c60e928e4a
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-25 18:46:08 +0000

    Calculated Big O Notation for circleOfNumbers.js

--- CodeSignal/circleOfNumbers.js
+++ CodeSignal/circleOfNumbers.js
@@ -1,12 +1,14 @@
 function solution(n, firstNumber) {
-  const half = n / 2;
-  let across = firstNumber + half;
-  if (across > n) {
-    across = firstNumber - half;
-  } else if (across === n) {
-    across = 0;
+  const half = n / 2; // 3 * 1 = 0(3) ~= 0(1)
+  let across = firstNumber + half; // 4 * 1 = 0(4) ~= 0(1)
+  if (across > n) { // 3 * 1 = 0(3) ~= 0(1)
+    across = firstNumber - half; // 4 * 1 = 0(4) ~= 0(1)
+  } else if (across === n) { // 3 * 1 = 0(3) ~= 0(1)
+    across = 0; // 1 * 1 = 0(1)
   }
-  return across;
+  return across; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(1) constant time
 
 solution(10, 2);
