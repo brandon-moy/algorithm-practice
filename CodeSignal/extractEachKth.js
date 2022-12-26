73023e27e176dc9a7fdd58cfdb9dd0ee52a62abc
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-26 19:48:58 +0000

    Calculated Big O Notation for extractEachKth.js

--- CodeSignal/extractEachKth.js
+++ CodeSignal/extractEachKth.js
@@ -1,14 +1,18 @@
 function solution(inputArray, k) {
-  var count = 1;
-  for (var i = 0; i < inputArray.length;) {
-    if ((i + count) % k === 0) {
-      inputArray.splice(i, 1);
-      count++;
+  var count = 1; // 1 * 1 = 0(1)
+  for (
+    var i = 0; // 1 * 1 = 0(1)
+    i < inputArray.length;) { // 2 * n = 0(2n) ~= 0(n)
+    if ((i + count) % k === 0) { // 3 * n = 0(3n) ~= 0(n)
+      inputArray.splice(i, 1); // 3 * n = 0(3n) ~= 0(n)
+      count++; // 2 * n = 0(2n) ~= 0(n)
     } else {
-      i++;
+      i++; // 2 * n = 0(2n) ~= 0(n)
     }
   }
-  return inputArray;
+  return inputArray; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution([1, 2, 3, 4], 4);
