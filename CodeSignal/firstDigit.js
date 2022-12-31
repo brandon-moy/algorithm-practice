5ce4e70e20f639b7af25a07bc930743626c08fec
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-31 20:30:53 +0000

    Calculated Big O Notation for firstDigit.js

--- CodeSignal/firstDigit.js
+++ CodeSignal/firstDigit.js
@@ -1,9 +1,14 @@
 function solution(inputString) {
-  for (let i = 0; i < inputString.length; i++) {
-    if (!isNaN(parseInt(inputString[i]))) {
-      return inputString[i];
+  for (
+    let i = 0; // 1 * 1 = 0(1)
+    i < inputString.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    if (!isNaN(parseInt(inputString[i]))) { // 4 * n = 0(4n) ~= 0(n)
+      return inputString[i]; // 2 * 1 = 0(2) ~= 0(1)
     }
   }
 }
+
+// Big O Notation: 0(n) linear time
 
 solution('q2qq');
