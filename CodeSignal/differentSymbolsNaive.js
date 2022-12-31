cc7258e1fedb306fdffc89ea40d1e6337acc396c
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-31 20:32:46 +0000

    Calculated Big O Notation for differentSymolsNaive.js

--- CodeSignal/differentSymbolsNaive.js
+++ CodeSignal/differentSymbolsNaive.js
@@ -1,13 +1,18 @@
 function solution(s) {
-  let count = 1;
-  const characters = [s[0]];
-  for (let i = 0; i < s.length; i++) {
-    if (!characters.includes(s[i])) {
-      count++;
-      characters.push(s[i]);
+  let count = 1; // 1 * 1 = 0(1)
+  const characters = [s[0]]; // 2 * 1 = 0(2) ~= 0(1)
+  for (
+    let i = 0; // ! * 1 = 0(1)
+    i < s.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    if (!characters.includes(s[i])) { // 4 * n = 0(4n) ~= 0(n)
+      count++; // 2 * n = 0(2n) ~= 0(n)
+      characters.push(s[i]); // 3 * n = 0(3n) ~= 0(n)
     }
   }
-  return count;
+  return count; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution('cabac');
