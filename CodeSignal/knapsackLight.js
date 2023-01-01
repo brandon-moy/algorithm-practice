9469bf9a6215d0fc570d281b024761ad72cb2771
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-01 20:17:32 +0000

    Calculated Big O Notation for knapsackLight.js

--- CodeSignal/knapsackLight.js
+++ CodeSignal/knapsackLight.js
@@ -1,17 +1,19 @@
 function solution(value1, weight1, value2, weight2, maxW) {
-  if (weight1 > maxW && weight2 > maxW) {
-    return 0;
-  } else if ((weight1 + weight2) <= maxW) {
-    return value1 + value2;
+  if (weight1 > maxW && weight2 > maxW) { // 6 * 1 = 0(6) ~= 0(1)
+    return 0; // 1 * 1 = 0(1)
+  } else if ((weight1 + weight2) <= maxW) { // 5 * 1 = 0(5) ~= 0(1)
+    return value1 + value2; // 3 * 1 = 0(3) ~= 0(1)
   } else {
-    if (weight1 <= maxW) {
-      var max = value1;
+    if (weight1 <= maxW) { // 3 * 1 = 0(3) ~= 0(1)
+      var max = value1; // 2 * 1 = 0(2) ~= 0(1)
     }
-    if (weight2 <= maxW && value2 > value1) {
-      max = value2;
+    if (weight2 <= maxW && value2 > value1) { // 6 * 1 = 0(6) ~= 0(1)
+      max = value2; // 2 * 1 = 0(2) ~= 0(1)
     }
-    return max;
+    return max; // 1 * 1 = 0(1)
   }
 }
+
+// Big O Notation: 0(1) constant time
 
 solution(15, 2, 20, 3, 2);
