c96d18139d25576e24b9f532809876d3dede193a
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-01 20:16:01 +0000

    Calculated Big O Notation for growingPlant.js

--- CodeSignal/growingPlant.js
+++ CodeSignal/growingPlant.js
@@ -1,15 +1,17 @@
 function solution(upSpeed, downSpeed, desiredHeight) {
-  let currentHeight = 0;
-  let days = 0;
-  while (currentHeight < desiredHeight) {
-    days++;
-    currentHeight += upSpeed;
-    if (currentHeight >= desiredHeight) {
-      return days;
+  let currentHeight = 0; // 1 * 1 = 0(1)
+  let days = 0; // 1 * 1 = 0(1)
+  while (currentHeight < desiredHeight) { // 2 * n = 0(2n) ~= 0(n)
+    days++; // 2 * n = 0(2n) ~- 0(n)
+    currentHeight += upSpeed; // 3 * n = 0(3n) ~= 0(n)
+    if (currentHeight >= desiredHeight) { // 3 * n = 0(3n) ~= 0(n)
+      return days; // 1 * 1 = 0(1)
     }
-    currentHeight -= downSpeed;
+    currentHeight -= downSpeed; // 3 * n = 0(3n) ~= 0(n)
 
   }
 }
+
+// Big O Notation: 0(n) linear time
 
 solution(100, 10, 900);
