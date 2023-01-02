83eaf1a8cb7e140b115a350f59adde48fe38f8d4
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-02 20:18:23 +0000

    Calculated Big O Notation for longestDigitsPrefix.js

--- CodeSignal/longestDigitsPrefix.js
+++ CodeSignal/longestDigitsPrefix.js
@@ -1,16 +1,21 @@
 function solution(inputString) {
-  let output = '';
+  let output = ''; // 1 * 1 - 0(1)
 
-  for (let i = 0; i < inputString.length; i++) {
-    const current = inputString[i];
+  for (
+    let i = 0; // 1 * 1 = 0(1)
+    i < inputString.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    const current = inputString[i]; // 3 * n = 0(3n) ~= 0(n)
 
-    if (!isNaN(current) && current !== ' ') {
-      output += current;
+    if (!isNaN(current) && current !== ' ') { // 5 * n = 0(5n) ~= 0(n)
+      output += current; // 3 * n = 0(3n) ~= 0(n)
     } else {
-      break;
+      break; // 1 * 1 = 0(1)
     }
   }
-  return output;
+  return output; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution('123a1');
