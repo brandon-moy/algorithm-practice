5a29d9209eaec7ccf14226fd8e20f1df5ea6a1fa
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-12 22:13:21 +0000

    Calculated Big O Notation for lineEncoding.js

--- CodeSignal/lineEncoding.js
+++ CodeSignal/lineEncoding.js
@@ -1,20 +1,25 @@
 function solution(s) {
-  let count = 1;
-  let output = '';
+  let count = 1; // 1 * 1 = 0(1)
+  let output = ''; // 1 * 1 = 0(1)
 
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === s[i + 1]) {
-      count++;
+  for (
+    let i = 0; // 1 * 1 = 0(1)
+    i < s.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    if (s[i] === s[i + 1]) { // 4 * n = 0(4n) ~= 0(n)
+      count++; // 2 * n = 0(2n) ~= 0(n)
     } else {
-      if (count > 1) {
-        output += count + s[i];
+      if (count > 1) { // 2 * n = 0(2n) ~= 0(n)
+        output += count + s[i]; // 5 * n = 0(5n) ~= 0(n)
       } else {
-        output += s[i];
+        output += s[i]; // 3 * n = 0(3n) ~= 0(n)
       }
-      count = 1;
+      count = 1; // 1 * 1 = 0(1)
     }
   }
-  return output;
+  return output; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution('aabbccc');
