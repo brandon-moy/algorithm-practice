63b2729c1edb87df4a3d05bf71c0e4acd1abbe44
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-12 22:10:34 +0000

    Calculated Big O Notation for isDigit.js

--- CodeSignal/isDigit.js
+++ CodeSignal/isDigit.js
@@ -1,7 +1,11 @@
 function solution(symbol) {
-  const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  if (!digit.includes(symbol)) return false;
-  return true;
+  const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; // 1 * 1 = 0(1)
+  if (!digit.includes(symbol)) { // 4 * 1 = 0(4) ~= 0(1)
+    return false; // 1 * 1 = 0(1)
+  }
+  return true; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(1) constant time
 
 solution('1');
