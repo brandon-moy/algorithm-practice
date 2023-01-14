c83e9d8a05c6ec613ef817589ea118fb5e96b7a4
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-14 20:11:15 +0000

    Calculated Big O Notation for deleteDigit.js

--- CodeSignal/deleteDigit.js
+++ CodeSignal/deleteDigit.js
@@ -1,13 +1,17 @@
 function solution(n) {
-  const options = [];
-  const s = n.toString();
-  for (let i = 0; i < s.length; i++) {
-    const change = s.replace(s[i], '');
-    options.push(parseInt(change));
+  const options = []; // 1 * 1 = 0(1)
+  const s = n.toString(); // 3 * 1 = 0(3) ~= 0(1)
+  for (let i = 0; // 1 * 1 = 0(1)
+    i < s.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    const change = s.replace(s[i], ''); // ? * n = 0(?n) ~= 0(n)
+    options.push(parseInt(change)); //  ? * n = 0(?n) ~= 0(n)
   }
 
-  const output = Math.max(...options);
-  return output;
+  const output = Math.max(...options); // 3 * 1 = 0(3) ~= 0(1)
+  return output; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(n) linear time
 
 solution(152);
