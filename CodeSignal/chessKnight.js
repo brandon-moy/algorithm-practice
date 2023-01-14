752a67ce00fac8a6cbb4607067d0d1a659d70c39
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-14 20:09:31 +0000

    Calculated Big O Notation for chessKnight.js

--- CodeSignal/chessKnight.js
+++ CodeSignal/chessKnight.js
@@ -1,19 +1,27 @@
 function solution(cell) {
-  let moves = 8;
-  if (cell[0] === 'a' || cell[0] === 'h') moves -= 4;
-  if (cell[0] === 'b' || cell[0] === 'g') moves -= 2;
-  if (cell[1] === '1' || cell[1] === '8') {
-    if (cell[0] === 'a' || cell[0] === 'h') {
-      moves -= 2;
-    } else if (cell[0] === 'g' || cell[0] === 'b') {
-      moves -= 3;
+  let moves = 8; // 1 * 1 = 0(1)
+  if (cell[0] === 'a' || cell[0] === 'h') { // 6 * 1 = 0(6) ~= 0(1)
+    moves -= 4; // 3 * 1 = 0(3) ~= 0(1)
+  }
+  if (cell[0] === 'b' || cell[0] === 'g') { // 6 * 1 = 0(6) ~= 0(1)
+    moves -= 2; // 3 * 1 = 0(3) ~= 0(1)
+  }
+  if (cell[1] === '1' || cell[1] === '8') { // 6 * 1 = 0(6) ~= 0(1)
+    if (cell[0] === 'a' || cell[0] === 'h') { // 6 * 1 = 0(6) ~= 0(1)
+      moves -= 2; // 3 * 1 = 0(3) ~= 0(1)
+    } else if (cell[0] === 'g' || cell[0] === 'b') { // 6 * 1 = 0(6) ~= 0(1)
+      moves -= 3; // 3 * 1 = 0(3) ~= 0(1)
     } else {
-      moves -= 4;
+      moves -= 4; // 3 * 1 = 0(3) ~= 0(1)
     }
   }
-  if (cell[1] === '2' || cell[1] === '7') moves -= 2;
+  if (cell[1] === '2' || cell[1] === '7') { // 6 * 1 = 0(6) ~= 0(1)
+    moves -= 2; // 3 * 1 = 0(3) ~= 0(1)
+  }
 
-  return moves;
+  return moves; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(1) constant time
 
 solution('a1');
