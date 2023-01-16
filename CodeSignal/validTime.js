a1ad563cddbb85a95f6c20b5d8a3d7492c7977dd
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-16 21:21:02 +0000

    Calculated Big O Notation for validTime.js

--- CodeSignal/validTime.js
+++ CodeSignal/validTime.js
@@ -1,10 +1,16 @@
 function solution(time) {
-  const split = time.split(':');
-  const hours = parseInt(split[0]);
-  const min = parseInt(split[1]);
-  if (hours > 23) return false;
-  if (min > 59) return false;
-  return true;
+  const split = time.split(':'); // 2 * 1 = 0(2) ~= 0(1)
+  const hours = parseInt(split[0]); // 3 * 1 = 0(3) ~= 0(1)
+  const min = parseInt(split[1]); // 3 * 1 = 0(3) ~= 0(1)
+  if (hours > 23) { // 2 * 1 = 0(2) ~= 0(1)
+    return false; // 1 * 1 = 0(1)
+  }
+  if (min > 59) { // 2 * 1 = 0(2) ~= 0(1)
+    return false; // 1 * 1 = 0(1)
+  }
+  return true; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(1) constant time
 
 solution('23:51');
