0e93577437adc06a02795fbe46fb9451b5bbee38
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-20 16:28:27 +0000

    Calculated Big O Notation for findEmailDomain.js

--- CodeSignal/findEmailDomain.js
+++ CodeSignal/findEmailDomain.js
@@ -1,7 +1,11 @@
 function solution(address) {
-  const split = address.split('@');
-  if (split.length === 2) return split[1];
-  return split[2];
+  const split = address.split('@'); // 4 * 1 = 0(4) ~= 0(1)
+  if (split.length === 2) { // 2 * 1 = 0(2) ~= 0(1)
+    return split[1]; // 1 * 1 = 0(1)
+  }
+  return split[2]; // 1 * 1 = 0(1)
 }
+
+// Big O Notation: 0(1) constant time
 
 solution('brandon@brandonmoy.com');
