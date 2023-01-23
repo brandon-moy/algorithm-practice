5f6ced9e7b4d69d0f181aceedcdac42c51ceb68c
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-23 20:30:32 +0000

    Calculated Big O Notation for sumUpNumbers.js

--- CodeSignal/sumUpNumbers.js
+++ CodeSignal/sumUpNumbers.js
@@ -1,21 +1,25 @@
 function solution(s) {
-  let sum = 0;
-  let tmp = '';
-  for (let i = 0; i < s.length; i++) {
-    if (isDigit(s[i])) {
-      tmp += s[i];
+  let sum = 0; // 1 * 1 = 0(1)
+  let tmp = ''; // 1 * 1 = 0(1)
+  for (let i = 0; // 1 * 1 = 0(1)
+    i < s.length; // 2 * n = 0(2n) ~= 0(n)
+    i++) { // 2 * n = 0(2n) ~= 0(n)
+    if (isDigit(s[i])) { // ? * n = 0(?n) ~= 0(n)
+      tmp += s[i]; // 3 * n = 0(3n) ~= 0(n)
     } else {
-      if (tmp) sum += parseInt(tmp);
-      tmp = '';
+      if (tmp) sum += parseInt(tmp); // 4 * n = 0(4n) ~= 0(n)
+      tmp = ''; // 1 * 1 = 0(1)
     }
   }
 
-  if (tmp) sum += parseInt(tmp);
-  return sum;
+  if (tmp) sum += parseInt(tmp); // 4 * n = 0(4n) ~= 0(n)
+  return sum; // 1 * 1 = 0(1)
 }
 
 function isDigit(c) {
   return c >= '0' && c <= '9';
 }
+
+// Big O Notation: 0(n) linear time
 
 solution('2 apples 2 oranges');
