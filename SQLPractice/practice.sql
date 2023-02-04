d4a8cca6f2fc965afc8529162d55c261f76a20b9
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-04 18:42:58 +0000

    Solution to question 14 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -87,3 +87,9 @@ patient_id,
 count(patient_id) as admissions
 from admissions
 where patient_id = '579';
+
+-- 14) Based on the cities that our patients live in, show unique cities that
+-- are in province_id 'NS'?
+select distinct(city) as unique_cities
+from patients
+where province_id = 'NS'
