f1327fd79815b1dc9682926e4c6ea886c3a5e1c4
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-04 18:36:58 +0000

    Solution to question 4from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -18,3 +18,10 @@ SELECT
   first_name
 FROM patients
 WHERE first_name like 'C%';
+
+-- Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
+SELECT
+  first_name,
+  last_name
+FROM patients
+WHERE weight between 100 and 120;
