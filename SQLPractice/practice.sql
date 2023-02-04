d7476c5b8f2d8003dc8a3bcb238f963513b53565
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-04 19:08:22 +0000

    Solution to question 19 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -129,7 +129,16 @@ where
 order by city asc
 
 -- MEDIUM QUESTIONS
--- Show unique birth years from patients and order them by ascending.
+-- 18) Show unique birth years from patients and order them by ascending.
 select distinct YEAR(birth_date)
 from patients
 order by birth_date asc
+
+-- 19) Show unique first names from the patients table which only occurs once in the list.
+-- For example, if two or more people are named 'John' in the first_name column
+-- then don't include their name in the output list. If only 1 person is named
+-- 'Leo' then include them in the output.
+select first_name
+from patients
+group by first_name
+having count(first_name) = 1
