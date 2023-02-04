c4048fe5be82776331c7de89931f506307959b53
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-04 18:41:50 +0000

    Solution to question 12 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -1,4 +1,4 @@
--- Show first name, last name, and gender of patients who's gender is 'M'
+-- 1) Show first name, last name, and gender of patients who's gender is 'M'
 SELECT
   first_name,
   last_name,
@@ -6,40 +6,40 @@ SELECT
 FROM patients
 WHERE gender = 'M';
 
--- Show first name and last name of patients who does not have allergies. (null)
+-- 2) Show first name and last name of patients who does not have allergies. (null)
 SELECT
   first_name,
   last_name
 FROM patients
 WHERE allergies is NULL;
 
--- Show first name of patients that start with the letter 'C'
+-- 3) Show first name of patients that start with the letter 'C'
 SELECT
   first_name
 FROM patients
 WHERE first_name like 'C%';
 
--- Show first name and last name of patients that weight within the range of 100
---  to 120 (inclusive)
+-- 4) Show first name and last name of patients that weight within the range of
+-- 100 to 120 (inclusive)
 SELECT
   first_name,
   last_name
 FROM patients
 WHERE weight between 100 and 120;
 
--- Update the patients table for the allergies column. If the patient's allergies
--- is null then replace it with 'NKA'
+-- 5) Update the patients table for the allergies column. If the patient's
+-- allergies is null then replace it with 'NKA'
 UPDATE patients
 set allergies = 'NKA'
 where allergies is null;
 
--- Show first name and last name concatinated into one column to show their full
---  name.
+-- 6) Show first name and last name concatinated into one column to show their
+-- full name.
 select concat(first_name, ' ', last_name)
 from patients
 
 
--- Show first name, last name, and the full province name of each patient.
+-- 7) Show first name, last name, and the full province name of each patient.
 -- Example: 'Ontario' instead of 'ON'
 select
   first_name,
@@ -49,12 +49,12 @@ from patients
   join province_names ON province_names.province_id = patients.province_id;
 
 
--- Show how many patients have a birth_date with 2010 as the birth year.
+-- 8) Show how many patients have a birth_date with 2010 as the birth year.
 select count(birth_date) as total_patients
 from patients
 where YEAR(birth_date) = 2010;
 
--- Show the first_name, last_name, and height of the patient with the greatest
+-- 9) Show the first_name, last_name, and height of the patient with the greatest
 -- height.
 select
 first_name,
@@ -62,14 +62,21 @@ last_name,
 max(height) as height
 from patients
 
--- Show all columns for patients who have one of the following patient_ids:
+-- 10) Show all columns for patients who have one of the following patient_ids:
 -- 1,45,534,879,1000
 select
 *
 from patients
 where patient_id in (1, 45, 534, 879, 1000)
 
--- Show the total number of admissions
+-- 11) Show the total number of admissions
 select
 count(patient_id) as admissions
 from admissions
+
+-- 12) Show all the columns from admissions where the patient was admitted and
+-- discharged on the same day.
+select
+*
+from admissions
+where admission_date = discharge_date
