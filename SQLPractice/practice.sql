4e63d20b9e53f1a767290de1cf079f28f9952b1f
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-04 18:36:12 +0000

    Solution to question 2 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -5,3 +5,10 @@ SELECT
   gender
 FROM patients
 WHERE gender = 'M';
+
+-- Show first name and last name of patients who does not have allergies. (null)
+SELECT
+  first_name,
+  last_name
+FROM patients
+WHERE allergies is NULL;
