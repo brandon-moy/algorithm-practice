61a7ea57578eaf22ca30ed9ca662148468e91e88
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-04 19:31:47 +0000

    Solution to question 20 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -142,3 +142,11 @@ select first_name
 from patients
 group by first_name
 having count(first_name) = 1
+
+-- 20) Show patient_id and first_name from patients where their first_name start
+-- and ends with 's' and is at least 6 characters long.
+select patient_id,
+first_name
+from patients
+where first_name like 's%s'
+and length(first_name) > 5;
