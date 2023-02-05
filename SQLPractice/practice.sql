ee4130d506ea4411beaf595f59da4e06db7d137a
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-05 19:42:26 +0000

    Solution to question #31 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -253,3 +253,12 @@ select
   concat(upper(last_name), ',', lower(first_name)) as new_name_format
 from patients
 order by first_name desc
+
+-- 31) Show the province_id(s), sum of height; where the total sum of its
+-- patient's height is greater than or equal to 7,000.
+select
+  province_id,
+  sum(height) as sum_height
+from patients
+group by province_id
+having sum(height) > 7000
