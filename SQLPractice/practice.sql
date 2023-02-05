c4894e82f124ba56a14d29fa71ddcc76b2034faa
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-05 19:47:20 +0000

    Solution to question #33 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -269,3 +269,12 @@ select
   (MAX(weight) - Min(weight)) as weight_delta
 from patients
 where last_name = 'Maroni'
+
+-- 33) Show all of the days of the month (1-31) and how many admission_dates
+-- occurred on that day. Sort by the day with most admissions to least admissions.
+select
+  day(admission_date) as day_number,
+  count(*) as number_of_admissions
+from admissions
+group by day_number
+order by number_of_admissions desc
