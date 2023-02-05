575ef76b511c86c2d14989bcde0844948813dbeb
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-05 19:57:39 +0000

    Solution to question #35 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -286,3 +286,22 @@ where patient_id = 542
 group by patient_id
 having
 	admission_date = max(admission_date)
+
+-- 35) Show patient_id, attending_doctor_id, and diagnosis for admissions that
+-- match one of the two criteria:
+-- 1. patient_id is an odd number and attending_doctor_id is either 1, 5, or 19.
+-- 2. attending_doctor_id contains a 2 and the length of patient_id is 3 characters.
+select
+  patient_id,
+  attending_doctor_id,
+  diagnosis
+from admissions
+where
+  (
+    attending_doctor_id IN (1, 5, 19)
+    and patient_id % 2 != 0
+  )
+  or (
+    attending_doctor_id like '%2%'
+    and len(patient_id) = 3
+  )
