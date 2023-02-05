42d8300fdcdb92f656cd7e366ca21af3a56e843f
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-05 19:50:40 +0000

    Solution to question #34 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -278,3 +278,11 @@ select
 from admissions
 group by day_number
 order by number_of_admissions desc
+
+-- 34) Show all columns for patient_id 542's most recent admission_date.
+select *
+from admissions
+where patient_id = 542
+group by patient_id
+having
+	admission_date = max(admission_date)
