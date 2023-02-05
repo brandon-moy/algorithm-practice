66cd8d181c1219ce5208f232b1a9adc58b5fe0fa
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-05 19:15:51 +0000

    Solution to question #28 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -225,3 +225,12 @@ select
   last_name,
   'Doctor' as role
 from doctors
+
+-- 28) Show all allergies ordered by popularity. Remove NULL values from query.
+select
+  allergies,
+  count(allergies) as total_diagnosis
+from patients
+where allergies is not null
+group by allergies
+order by total_diagnosis desc
