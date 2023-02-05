dc5c48826e04353aa59dcbc0eca4e7d02405a510
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-05 19:13:42 +0000

    Solution to question #27 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -211,3 +211,17 @@ group by city
 order by
   total_patients desc,
   city asc
+
+-- 27) Show first name, last name and role of every person that is either
+-- patient or doctor. The roles are either "Patient" or "Doctor"
+select
+  first_name,
+  last_name,
+  'Patient' as role
+from patients
+union all
+select
+  first_name,
+  last_name,
+  'Doctor' as role
+from doctors
