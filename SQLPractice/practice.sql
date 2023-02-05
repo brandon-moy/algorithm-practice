f45e4eafb2f1599f196e96678151891e1e7eba7f
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-05 20:03:48 +0000

    Solution to question #37 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -316,3 +316,14 @@ select
 from admissions
   join doctors ph on ph.doctor_id = admissions.attending_doctor_id
 group by attending_doctor_id
+
+-- 37) For each doctor, display their id, full name, and the first and last
+-- admission date they attended.
+select
+  doctor_id,
+  concat(first_name, ' ', last_name) as full_name,
+  min(admission_date) as first_admission,
+  max(admission_date) as last_admission
+from admissions
+  join doctors ph on ph.doctor_id = admissions.attending_doctor_id
+group by attending_doctor_id
