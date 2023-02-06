b22fb3785d65877660b22e235b30ae9b9ab92e33
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-06 20:12:34 +0000

    Solution to question # 44 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -412,3 +412,19 @@ select
     else 0
   end as isObese
 from patients
+
+-- 44) Show patient_id, first_name, last_name, and attending doctor's specialty.
+-- Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
+-- Check patients, admissions, and doctors tables for required information.
+
+select
+  patients.patient_id,
+  patients.first_name,
+  patients.last_name,
+  doctors.specialty
+from patients
+  join admissions on admissions.patient_id = patients.patient_id
+  join doctors on doctors.doctor_id = admissions.attending_doctor_id
+where
+  admissions.diagnosis = 'Epilepsy'
+  and doctors.first_name = 'Lisa'
