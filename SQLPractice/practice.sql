42ed64e7aae655085d3552eb62c125a26435f836
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-06 20:07:06 +0000

    Solution to question # 43 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -395,3 +395,20 @@ select
 from patients
 group by weight_group
 order by weight_group desc
+
+-- 43) Show patient_id, weight, height, isObese from the patients table.
+
+-- Display isObese as a boolean 0 or 1.
+-- Obese is defined as weight(kg)/(height(m)2) >= 30.
+-- weight is in units kg.
+-- height is in units cm.
+
+select
+  patient_id,
+  weight,
+  height,
+  case
+    when weight / power(height / 100.0, 2) >= 30 then 1
+    else 0
+  end as isObese
+from patients
