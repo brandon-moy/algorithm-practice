779ef2a8c884a3e53eb5a8a3c13f531891175a49
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-06 18:37:48 +0000

    Solution to Divisors exercise from practicepython.org

---
+++ PyPractice/divisors.py
@@ -0,0 +1,16 @@
+# Divisors
+
+# Create a program that asks the user for a number and then prints out a list of
+# all the divisors of that number. (If you don’t know what a divisor is, it is a
+# number that divides evenly into another number. For example, 13 is a divisor of
+# 26 because 26 / 13 has no remainder.)
+
+num = int(input('Please choose a number to divide: '))
+
+lr = list(range(1, num+1))
+divisors = list()
+
+for number in lr :
+    if num % number == 0:
+        divisors.append(number)
+print(divisors)
