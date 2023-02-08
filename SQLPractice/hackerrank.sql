2c58ab045c650bc87898a04ee0987844aa9ddd5a
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-08 19:39:39 +0000

    Solution to question # 2 from HackerRank sql

--- SQLPractice/hackerrank.sql
+++ SQLPractice/hackerrank.sql
@@ -1,2 +1,13 @@
 -- 1) Query all columns for all American cities in the CITY table with
 -- populations larger than 100000. The CountryCode for America is USA.
+
+select *
+from city
+where countrycode = 'USA' and population > 100000
+
+-- 2) Query the NAME field for all American cities in the CITY table with
+-- populations larger than 120000. The CountryCode for America is USA.
+
+select name
+from city
+where countrycode = 'USA' and population > 120000
