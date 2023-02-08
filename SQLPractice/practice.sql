c85b907a027031edcb3ee90447c546e9d9375dec
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-08 02:56:44 +0000

    Solution to question # 46 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -446,3 +446,22 @@ SELECT
   )
 FROM patients
   join admissions on admissions.patient_id = patients.patient_id
+
+
+-- 46) Each admission costs $50 for patients without insurance, and $10 for
+-- patients with insurance. All patients with an even patient_id have insurance.
+
+-- Give each patient a 'Yes' if they have insurance, and a 'No' if they don't
+-- have insurance. Add up the admission_total cost for each has_insurance group.
+
+SELECT
+  case
+  	when patient_id % 2 = 0 then 'Yes'
+    else 'No'
+    end as has_insurance,
+  case
+  	when patient_id % 2 = 0 THEN count(*) * 10
+  	else count(*) * 50
+    end as cost_after_insurance
+FROM admissions
+group by has_insurance
