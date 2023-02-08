88b3334916c2caf6f91393f5e93ce7208eb530e7
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-08 03:30:56 +0000

    Solution to final question # 51 from sql-practice.com

--- SQLPractice/practice.sql
+++ SQLPractice/practice.sql
@@ -529,3 +529,11 @@ select
   count(*) - lag(count(8), 1) over() as admissions_count_change
 from admissions
 group by admission_date
+
+-- 51) Sort the province names in ascending order in such a way that the
+-- province 'Ontario' is always on top.
+
+select province_name
+from province_names
+order by
+	(case when province_name = 'Ontario' then 0 else 1 end)
