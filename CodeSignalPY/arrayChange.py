ed0b35237edd22db16dd86b390a9244b14543133
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-17 02:53:08 +0000

    Solution to ArrayChange to ArcadeIntro from CodeSignal in Python

---
+++ CodeSignalPY/arrayChange.py
@@ -0,0 +1,8 @@
+def solution(inputArray):
+    output = 0
+    for i in range(1, len(inputArray)):
+        if inputArray[i] <= inputArray[i - 1]:
+            f = (inputArray[i - 1] - inputArray[i]) + 1
+            inputArray[i] = inputArray[i - 1] + 1
+            output += f
+    return output
