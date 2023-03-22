36f44727dff9136dd050ab9a0408307af7124650
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-03-22 01:34:36 +0000

    Solution to differentSquares from ArcadeIntro, CodeSignal

---
+++ CodeSignal/differentSquares.js
@@ -0,0 +1,36 @@
+function solution(matrix) {
+  var d = [];
+  for (var i = 1; i < matrix.length; i++) {
+    for (var j = 1; j < matrix[i].length; j++) {
+      if (not(i, j)) {
+        d.push([
+          [
+            matrix[i - 1][j - 1],
+            matrix[i - 1][j]
+          ], [
+            matrix[i][j - 1],
+            matrix[i][j]
+          ]
+        ]);
+      }
+    }
+  }
+
+  function not(i, j) {
+    return d.every(c => {
+      return c[0][0] !== matrix[i - 1][j - 1] ||
+        c[0][1] !== matrix[i - 1][j] ||
+        c[1][0] !== matrix[i][j - 1] ||
+        c[1][1] !== matrix[i][j];
+    });
+  }
+  return d.length;
+}
+
+const matrix = [[1, 2, 1],
+  [2, 2, 2],
+  [2, 2, 2],
+  [1, 2, 3],
+  [2, 2, 1]];
+
+solution(matrix);
