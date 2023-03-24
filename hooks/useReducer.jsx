3dc372c15c80dfb0e28f3ae08674ad0d4e456dd0
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-03-24 02:31:28 +0000

    Created function and notes for useReducer hook

---
+++ hooks/useReducer.jsx
@@ -0,0 +1,35 @@
+// Similar to set state but in a redux pattern - dispatch actions to reducer
+// function which then determines what to do with the next state
+
+// helps to manage state as application grows
+import { React, useReducer } from 'react';
+
+// reducer function takes state and action and computers what to do with the
+// next state
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    default:
+      throw new Error();
+  }
+}
+
+// eslint-disable-next-line
+function App() {
+  // returns an array of two values
+  // value of state to show in UI
+  // function used to dispatch an action
+  // hook takes initial state as second argument
+  const [state, dispatch] = useReducer([reducer, 0]);
+
+  // action is an option that has a type (string) and a data payload
+  return (
+    <>
+      Count: {state}
+      <button onClick={() => dispatch({ type: 'decrement' })} />
+    </>
+  );
+}
