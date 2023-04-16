a81701bb55b991abc8da5bea6231bb32616d134f
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-04-16 16:23:44 +0000

    Modified almostIncreasingSequence test for positive integers and negative integers

--- tests/almostIncreasingSequence.test.js
+++ tests/almostIncreasingSequence.test.js
@@ -2,7 +2,7 @@ import almostIncreasingSequence from '../CodeSignal/almostIncreasingSequence.js'
 import { expect } from 'chai';
 
 describe('Given a sequence of integers in an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array', () => {
-  it('Returns a boolean if description can be achieved for array [1, 3, 2, 1]', () => {
+  it('Returns a boolean if description can be achieved for array of positive integers', () => {
     const testOne = [1, 3, 2, 1];
     const testOneResult = almostIncreasingSequence(testOne);
 
@@ -10,8 +10,8 @@ describe('Given a sequence of integers in an array, determine whether it is poss
     expect(testOneResult).to.equal(false);
   });
 
-  it('Returns a boolean if description can be achieved for array [1, 3, 2]', () => {
-    const testTwo = [1, 3, 2];
+  it('Returns a boolean if description can be achieved for array of negative integers', () => {
+    const testTwo = [-1, -3, -2];
     const testTwoResult = almostIncreasingSequence(testTwo);
 
     expect(testTwoResult).to.be.a('boolean');
