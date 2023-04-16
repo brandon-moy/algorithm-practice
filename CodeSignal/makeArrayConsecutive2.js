63299a7b8d4b66650c773806ad4a8af181115b86
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-04-16 16:30:29 +0000

    Modified makeAreaConsecutive2 code to return null if NaN and added test to check

--- CodeSignal/makeArrayConsecutive2.js
+++ CodeSignal/makeArrayConsecutive2.js
@@ -1,4 +1,7 @@
 export default function makeAreaConsecutive2(statues) {
+  for (var i = 0; i < statues.length; i++) {
+    if (typeof statues[i] !== 'number') return null;
+  }
   var min = Math.min(...statues);
   var max = Math.max(...statues);
   var count = max - min - statues.length + 1;
--- tests/makeAreaConsecutive2.test.js
+++ tests/makeAreaConsecutive2.test.js
@@ -2,7 +2,7 @@ import makeAreaConsecutive2 from '../CodeSignal/makeArrayConsecutive2.js';
 import { expect } from 'chai';
 
 describe('Given an array of statues, return the number of statues needed to arrange them smallest to largest where the size difference is only 1 between each statue', () => {
-  it('Returns the correct number of statues needed for array [6, 2, 3, 8]', () => {
+  it('Returns the correct number of statues needed for array of positive integers', () => {
     const testOne = [6, 2, 3, 8];
     const testOneResult = makeAreaConsecutive2(testOne);
 
@@ -10,19 +10,27 @@ describe('Given an array of statues, return the number of statues needed to arra
     expect(testOneResult).to.equal(3);
   });
 
-  it('Returns the correct number of statues needed for array [0, 3]', () => {
-    const testTwo = [0, 3];
+  it('Returns the correct number of statues needed for array of negative integers', () => {
+    const testTwo = [0, -3];
     const testTwoResult = makeAreaConsecutive2(testTwo);
 
     expect(testTwoResult).to.be.a('number');
     expect(testTwoResult).to.equal(2);
   });
 
-  it('Returns the correct number of statues needed for array [5, 4, 6]', () => {
+  it('Returns the correct number of statues needed for array of positive integers', () => {
     const testThree = [5, 4, 6];
     const testThreeResult = makeAreaConsecutive2(testThree);
 
     expect(testThreeResult).to.be.a('number');
     expect(testThreeResult).to.equal(0);
+  });
+
+  it('Returns null if any input is not a number', () => {
+    const testThree = [5, 'e', 6];
+    const testThreeResult = makeAreaConsecutive2(testThree);
+
+    expect(testThreeResult).to.be.a('null');
+    expect(testThreeResult).to.equal(null);
   });
 });
